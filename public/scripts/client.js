33ce0cee20532770ea804a95e2b482349e62b01a
William <williamzak0@gmail.com>
2021-01-09 11:50:06 -0500

    titles can be added and removed

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,45 +1,102 @@
-const request = require('request');
 
 // Replace spaces in search with + to work in url
-const replaceSpaces = (input) => {
+const replaceSpaces = function(input) {
   let search = input.trim().split(" ").join("+");
   return search;
 };
 
-// Fetch movie titles from the OMDB API as an array of objects
-const searchTitle = (input) => {
-  const movie = replaceSpaces(input);
-
-  request(`http://www.omdbapi.com/?s=${movie}&apikey=30f52383`, (err, res, body) => {
-    if (err) return console.log("Error!");
-
-    const { Search } = JSON.parse(body);
-    const searchList = Search.slice(0, 6);
-    return searchList;
-  })
-}
-
 // Creates individual search result
-const createMovieElement = (movieInfo) => {
+const createMovieElement = function(movieInfo) {
   let $movie = $(`
-    <article>
+    <li>
       <div class="movieInfo">
-        <p>${movieInfo.Title}</p>
-        <p>(${movieInfo.Year})</p>
+        <p class="${movieInfo.imdbID}">${movieInfo.Title} (${movieInfo.Year})</p>
       </div>
       <div class="nominateBtn">
-        <button type="submit" id="nominateMovie">Nominate</button>
+        <button type="submit" class="nominateMovie" id="${movieInfo.imdbID}">Nominate</button>
       </div>
-    </article>
+    </li>
   `);
 
   return $movie;
 }
 
 // Render top 6 results to results box
-const renderMovies = (data) => {
-  for (let movie of data) {
-    let output = createMovieElement(movie);
+const renderMovies = function(data) {
+  for (let n = 0; n <= 5; n++) {
+    let output = createMovieElement(data[n]);
     $(`#results`).append(output);
   }
 }
+
+const renderNomination = function(nomInfo) {
+  let $nom = $(`
+    <li>
+      <div class="nomInfo">${nomInfo}</div>
+      <div class="removeBtn">
+        <button type="submit" class="removeNom">Remove</button>
+      </div>
+    </li>
+  `)
+  console.log(nomInfo)
+  $("#nominations").append($nom);
+}
+
+let listItems = 0;
+
+$(document).ready(function() {
+
+  const loadResults = function(req) {
+    const movie = replaceSpaces(req);
+
+    $(function() {
+      $.ajax(`http://www.omdbapi.com/?s=${movie}&apikey=30f52383`, { method: "GET"})
+      .then(function(movies) {
+        $("#results").empty();
+        renderMovies(movies.Search)
+      })
+    })
+  }
+
+  $("#search").on('click', function(event) {
+    event.preventDefault();
+
+    const searchText = $("#searchBar").serialize();
+    const searchTextRaw = $("#searchBar").val();
+    $("#searchBar").val("");
+
+    loadResults(searchTextRaw);
+  })
+
+})
+
+// To add a movie to the nomination list
+$(document).on('click', '#results .nominateMovie', function(event) {
+  event.preventDefault();
+
+  let imdbID = this.id;
+
+  const title = $(`.${imdbID}`).html();
+
+  listItems += 1;
+
+  if (listItems < 6) {
+    $("#nomErr").html("")
+    renderNomination(title);
+  } else {
+    $("#nomErr").html("🚨 You may only nominate 5 movies maximum 🚨")
+    $("#nomErr").slideDown("slow");
+  }
+})
+
+// To remove a nomination
+$(document).on('click', '#nominations .removeNom', function(event) {
+  event.preventDefault();
+  if (listItems < 6) {
+    $("#nomErr").html("")
+  }
+  listItems -= 1;
+  console.log(listItems)
+
+  $(this).closest('li').remove();
+})
---
+++ server/index.js
@@ -0,0 +1,11 @@
+const PORT          = 8080;
+const express       = require("express");
+const bodyParser    = require("body-parser");
+const app           = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log("Listening on port " + PORT);
+});
